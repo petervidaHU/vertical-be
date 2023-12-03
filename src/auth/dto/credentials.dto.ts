2ae8d3d181d6b35f35781d88a6ab29a5910c1259
peter_vida <peter_vida1@epam.com>
2023-12-03 20:37:36 +0100

    bugfix

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,4 +1,18 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CredentialsDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(20)
   password: string;
 }
