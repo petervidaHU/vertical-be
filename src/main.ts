0c3c0b0c3f7836bf9b5efbcaceb0a6193762500b
peter_vida <peter_vida1@epam.com>
2024-01-11 22:02:32 +0100

    update cors

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://vertical-e5vyu48xb-petervidahu.vercel.app/',
-    ],
+    origin: ['http://localhost:3001', 'https://vertical-fe-beryl.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type',
     preflightContinue: false,
